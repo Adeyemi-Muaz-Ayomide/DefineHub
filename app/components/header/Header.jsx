a3a08791ca543deec04686c09e817210f27ff540
iAmMuadh <adeyemimuaz1@gmail.com>
2023-10-11 23:37:42 +0100

    input style added

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -3,7 +3,7 @@ import "animate.css";
 export default function Header() {
   return (
     <header className="mb-10 flex flex-col place-items-center gap-4 text-center">
-      <h1 className="animate__animated animate__bounceInDown animate__delay-0.7s max-w-full mt-20 overflow-hidden truncate whitespace-nowrap bg-gradient-to-r from-blue-600 to-white bg-clip-text px-4 pb-4 text-5xl font-extrabold text-transparent sm:text-6xl xl:text-7xl">
+      <h1 className="animate__animated animate__bounceInDown animate__delay-0.7s max-w-full mt-16 overflow-hidden truncate whitespace-nowrap bg-gradient-to-r from-blue-600 to-white bg-clip-text px-4 pb-4 text-5xl font-extrabold text-transparent sm:text-6xl xl:text-7xl">
         DefineHub
       </h1>
       <h3 className="animate__animated animate__bounceInLeft animate__delay-0.8s px-4 text-lg font-light text-gray-400 sm:text-2xl">
--- app/components/main/Main.jsx
+++ app/components/main/Main.jsx
@@ -1,5 +1,18 @@
+import { GoSearch } from "react-icons/go";
+import "animate.css";
+
 export default function Main() {
   return (
-    <div>Main</div>
-  )
+    <form className="animate__animated animate__fadeIn mx-auto w-full max-w-5xl px-4">
+      <div className="relative">
+        <GoSearch className="absolute mt-5 ml-3 bg-gray-700 h-5 w-5 text-gray-500" />
+        <input
+          type="search"
+          className="w-full block rounded-lg pl-10 border border-gray-600 bg-gray-700 py-4   text-gray-300 transition-all focus:border-blue-500 focus:outline-none focus:ring-blue-500 "
+          placeholder="Search for a word..."
+        />
+        <button className="button">Search</button>
+      </div>
+    </form>
+  );
 }
